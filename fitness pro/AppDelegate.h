b83d682a016ee18ce35e34747760eeb4c8afec8d
James Bray <jrbrayjr2006@gmail.com>
2014-11-11 14:28:33 -0600

    Initial Commit

---
+++ fitness pro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fitness pro
+//
+//  Created by JAMES BRAY on 11/11/14.
+//  Copyright (c) 2014 Jaydot2 LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
